cd67a55a325dcea574b577116d016d71c1de0845
Ari Koumis <ariskoumis@gmail.com>
2019-05-29 14:53:18 -0700

    Modified test endpoint to send json instead, and added  makeJsonString helper w/ example.

--- Projects/Protoproject/main/Source.cpp
+++ Projects/Protoproject/main/Source.cpp
@@ -2,13 +2,29 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <WiFi.h>
+#include <vector>
 #include "Source.h"
 #include "Arduino.h"
 #include "EEPROM.h"
 #include "constants.h"
+#include <iostream>
 
+using namespace std;
 
+String makeJsonString(vector<String>& keys, vector<String>& vals);
 
+void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len){
+ 
+  if(type == WS_EVT_CONNECT){
+ 
+    Serial.println("Websocket client connection received");
+    client->text("Hello from ESP32 Server");
+ 
+  } else if(type == WS_EVT_DISCONNECT){
+    Serial.println("Client disconnected");
+ 
+  }
+}
 
 void initServer(AsyncWebServer* server, ParamsStruct* params) {
     
@@ -28,12 +44,12 @@ void initServer(AsyncWebServer* server, ParamsStruct* params) {
 
     // Connect to the Rover's AP
     WiFi.config(Ip, Gateway, NMask);
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        delay(500);
-        printf("Connecting to WiFi... ");
-    }
+    // WiFi.begin(ssid, password);
+    // while (WiFi.status() != WL_CONNECTED)
+    // {
+    //     delay(500);
+    //     printf("Connecting to WiFi... ");
+    // }
     printf("\nConnected to %s\n", ssid);
 
     AsyncEventSource events("/events");
@@ -52,25 +68,43 @@ void initServer(AsyncWebServer* server, ParamsStruct* params) {
         strcpy(params->name, request->arg("name").c_str());
         request->send(200, "text/plain", "Success");
     });
-    
-    
-    /* SSE Example.
-        - SSEs will be used to continuously send data that was
-        not necessarily requested by mission control
-        (e.g. temperature, something we should send periodically)
 
-        - Once mission control declares the ESPs IP address at a certain 
-        endpoint to be an EventSource, the ESP can trigger events on the web
-        interface, which the web interface can attach event listeners to
-        (similar to how we are attaching event listeners for when we recieve
-        XHRs to /update_name above, allowing us to do things when we recieve an 
-        XHR).
-        - Below's example is an example of sending SSEs when mission control
-        declares our ip address and endpoint (e.g. 192.168.4.1/events) to be
-        an event source.
-        - More info on this concept here: 
-            https://developer.mozilla.org/en-US/docs/Web/API/EventSource
-    */
+    server->on("/test", HTTP_POST, [=](AsyncWebServerRequest *request) {
+        //declare individual vectors for your keys and values. 
+        vector <String> keys, vals;
+        keys.push_back("key1");
+        keys.push_back("key2");
+        keys.push_back("key3");
+        //even if the values are ints, please convert them to strings (hacky, sorry.)
+        vals.push_back("val1");
+        vals.push_back("val2");
+        vals.push_back("val3");
+
+        //call the helper
+        String json = makeJsonString(keys, vals);
+
+        //send
+        request->send(200, "application/json", json);
+    });
+    
+    
+    // /* SSE Example.
+    //     - SSEs will be used to continuously send data that was
+    //     not necessarily requested by mission control
+    //     (e.g. temperature, something we should send periodically)
+
+    //     - Once mission control declares the ESPs IP address at a certain 
+    //     endpoint to be an EventSource, the ESP can trigger events on the web
+    //     interface, which the web interface can attach event listeners to
+    //     (similar to how we are attaching event listeners for when we recieve
+    //     XHRs to /update_name above, allowing us to do things when we recieve an 
+    //     XHR).
+    //     - Below's example is an example of sending SSEs when mission control
+    //     declares our ip address and endpoint (e.g. 192.168.4.1/events) to be
+    //     an event source.
+    //     - More info on this concept here: 
+    //         https://developer.mozilla.org/en-US/docs/Web/API/EventSource
+    // */
     
     events.onConnect([](AsyncEventSourceClient *client) {
       if(client->lastId())
@@ -89,8 +123,12 @@ void initServer(AsyncWebServer* server, ParamsStruct* params) {
       delay(1000);
     });
 
-    //Attach event source to the server.
+    // //Attach event source to the server.
     server->addHandler(&events);
+
+    AsyncWebSocket ws("/ws");
+    ws.onEvent(onWsEvent);
+    server->addHandler(&ws);
 
     //Start server.
     server->begin();
@@ -121,4 +159,18 @@ int EEPROMCount(int addr)
     EEPROM.write(addr, data);
     EEPROM.commit();
     return data;
+}
+
+String makeJsonString(vector<String>& keys, vector<String>& vals) {
+    String json = "{";
+    
+    for (int i=0; i<keys.size(); i++) {
+      if (i) json += ",";
+      json += ("\"" + keys[i] + "\":");
+      json += ("\"" + vals[i] + "\"");
+    }
+    
+    json += "}";
+
+    return json;
 }
